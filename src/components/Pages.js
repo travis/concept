c8d29cfb252657b7062a4a870f99ba8890fbe6a5
Travis Vachon <travis.vachon@gmail.com>
2020-01-21 21:06:31 -0800

    Get name editing working
    Switch to "useDebounce" paradigm for saving.

--- src/components/Pages.js
+++ src/components/Pages.js
@@ -1,16 +1,18 @@
 import React, {useRef, useContext, useCallback, useState, useEffect} from 'react'
 import Box from '@material-ui/core/Box';
+import TextField from '@material-ui/core/TextField';
 import { makeStyles } from '@material-ui/core/styles';
 import { schema} from 'rdf-namespaces';
-import { debounce } from 'throttle-debounce';
+import { useDebounce } from 'use-debounce';
 
 import 'codemirror/lib/codemirror.css';
 import 'tui-editor/dist/tui-editor.min.css';
 import 'tui-editor/dist/tui-editor-contents.min.css';
-import { Editor } from '@toast-ui/react-editor'
+import { Editor } from '@toast-ui/react-editor';
 
-import WorkspaceContext from "../context/workspace"
-import PageDrawer from './PageDrawer'
+import WorkspaceContext from "../context/workspace";
+import PageDrawer from './PageDrawer';
+
 
 const useStyles = makeStyles(theme => ({
   saving: {
@@ -20,36 +22,70 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
+function PageName({page, updatePage}){
+  const [editing, setEditing] = useState(false);
+  const [name, setName] = useState(page.getString(schema.name));
+  useEffect(() => setName(page.getString(schema.name)), [page])
+  const save = useCallback(async function(value) {
+    if (value && (value !== page.getString(schema.name))) {
+      page.setLiteral(schema.name, value);
+      await updatePage(page);
+    }
+  }, [page]);
+
+  const [debouncedName] = useDebounce(name, 1000);
+  useEffect(() => {
+    save(debouncedName)
+  }, [debouncedName])
+
+  return editing ? (
+    <TextField label="Page Name" variant="standard" autoFocus
+               value={name}
+               onKeyDown={(e) => (e.key === 'Enter') && setEditing(false)}
+               onChange={(e) => setName(e.target.value)}/>
+  ) : (
+    <h3 onClick={() => setEditing(true)}>{name}</h3>
+  );
+}
+
 function Page({page, updatePage, deletePage, className}){
   const classes = useStyles();
   const [saving, setSaving] = useState(false);
-  const editorRef = useRef();
-  const onChange = useCallback(async function() {
-    const text = editorRef.current && editorRef.current.getInstance().getValue();
-    if (text && (text !== page.getString(schema.text))) {
-      page.setLiteral(schema.text, text)
+
+  const [text, setText] = useState(page.getString(schema.text));
+  const saveText = useCallback(async (value) => {
+    if (value !== page.getString(schema.text)) {
+      page.setLiteral(schema.text, value)
       setSaving(true)
       await updatePage(page)
       setSaving(false)
     }
-  }, [page, editorRef, updatePage]);
+  }, [page])
+
+  const [debouncedText] = useDebounce(text, 1000);
   useEffect(() => {
-    if (editorRef.current){
-      editorRef.current.getInstance().setMarkdown(page.getString(schema.text));
-    }
-  }, [page, editorRef]);
+    saveText(debouncedText)
+  }, [debouncedText])
+
+  const editorRef = useRef();
+  useEffect(() => {
+    const value = page.getString(schema.text);
+    setText(value)
+    editorRef.current.getInstance().setMarkdown(value);
+
+  }, [page, editorRef])
   return (
     <div className={className}>
-      <h3>{page.getString(schema.name)}</h3>
+      <PageName page={page} updatePage={updatePage}/>
       <p className={classes.saving}>{saving && "Saving..."}</p>
       <Editor
         ref={editorRef}
-        initialValue={page.getString(schema.text)}
+        initialValue={text}
         previewStyle="tab"
         height="600px"
         initialEditType="markdown"
         useCommandShortcut={true}
-        onChange={debounce(1000, onChange)}
+        onChange={() => editorRef.current && setText(editorRef.current.getInstance().getValue())}
       />
     </div>
   )
