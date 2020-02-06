898095a5418f93f836db949a6d6ceaf2cf569985
Travis Vachon <travis.vachon@gmail.com>
2020-02-05 16:30:14 -0800

    Fix missing deps, remove tripledoc

--- src/context/auth.js
+++ src/context/auth.js
@@ -1,54 +1,20 @@
 import React, {useEffect, useState, createContext} from 'react';
 import auth from 'solid-auth-client';
-import { fetchDocument } from 'tripledoc';
 
 const AuthContext = createContext({})
 
 const {Provider} = AuthContext;
 
 export const AuthProvider = (props) => {
-  const [sessionLoaded, setSessionLoaded] = useState(false)
-  const [session, setSession] = useState(null)
-  async function establishSession() {
-    const sesh = await auth.currentSession()
-    setSession(sesh)
-    setSessionLoaded(true)
-  }
-  useEffect(() => {
-    establishSession()
-  }, [])
-
   async function logIn() {
     const session = await auth.popupLogin({popupUri: "/popup.html"})
-    setSession(session)
   }
   async function logOut() {
     await auth.logout()
-    setSession(null)
   }
 
-  const [currentUserLoaded, setCurrentUserLoaded] = useState(false)
-  const [currentUser, setCurrentUser] = useState(null)
-  useEffect(() => {
-    const loadCurrentUser = async () => {
-      if (sessionLoaded) {
-        if (session) {
-          const webIdDoc = await fetchDocument(session.webId)
-          setCurrentUser(webIdDoc.getSubject(session.webId))
-        } else {
-          setCurrentUser(null)
-        }
-        setCurrentUserLoaded(true)
-      }
-    }
-    loadCurrentUser()
-  }, [sessionLoaded, session])
-
   return (
-    <Provider {...props} value={{
-      session, sessionLoaded,
-      currentUser, currentUserLoaded,
-      logIn, logOut}} />
+    <Provider {...props} value={{logIn, logOut}} />
   )
 }
 
