8a366f1c02487f9da5b230fba9d837d2d7d269ca
Travis Vachon <travis.vachon@gmail.com>
2020-04-01 19:57:14 -0700

    add link to github

--- src/components/SocialIcons.js
+++ src/components/SocialIcons.js
@@ -4,6 +4,7 @@ import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
 import Link from '@material-ui/core/Link';
 import Twitter from '@material-ui/icons/Twitter';
+import GitHub from '@material-ui/icons/GitHub';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles(theme => ({
@@ -19,11 +20,21 @@ export default function SocialIcons(){
   const classes = useStyles()
   return (
     <Grid container className={classes.social}>
-      <Grid item xs={12}>
+      <Grid item xs={5}>
+      </Grid>
+      <Grid item xs={1}>
         <Link href="https://twitter.com/useconceptart" target="_blank"
               className={classes.socialIcon}>
           <Twitter />
         </Link>
+      </Grid>
+      <Grid item xs={1}>
+        <Link href="https://github.com/travis/concept" target="_blank"
+              className={classes.socialIcon}>
+          <GitHub />
+        </Link>
+      </Grid>
+      <Grid item xs={5}>
       </Grid>
     </Grid>
   )
