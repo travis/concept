c7a27fd8a441c98657fecab96f1cf037cb8a2309
Travis Vachon <travis.vachon@gmail.com>
2021-01-26 11:47:26 -0800

    add notice on landing page about deprecation

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -42,6 +42,30 @@ function LandingPage() {
         </Grid>
       </Grid>
       <Grid container>
+        <Grid item xs={4}>
+        </Grid>
+        <Grid item xs={4}>
+          <h1>Hello Friend!</h1>
+          <h2>
+            I'm sorry to let you know that Concept is no longer supported or
+            actively developed.
+          </h2>
+          <h2>
+            But good news!
+          </h2>
+          <h2>
+            This code lives on (in some
+            cases literally) in <a href="https://github.com/itme/itme.online">itme.online</a>,
+                             a <a href="https://www.grantfortheweb.org/">Grant for the Web</a> supported project from <a href="https://itme.company">itme</a>.
+          </h2>
+          <h2>
+            If you're interested in following along, please subscribe to&nbsp;
+            <a href="https://itme.press/">our blog</a> or <a href="https://tinyletter.com/itme">newsletter</a> for
+              the latest updates.
+          </h2>
+        </Grid>
+      </Grid>
+      <Grid container>
         <Grid item xs={3}>
         </Grid>
         <Grid item xs={3}>
